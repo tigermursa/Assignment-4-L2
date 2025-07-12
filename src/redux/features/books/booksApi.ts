759fcb9ce5e8717657a98f6e605df2c535c22527
Mursalin <mursalin2311@gmail.com>
2025-07-13 03:07:37 +0600

    redux and RTK setup done

---
+++ src/redux/features/books/booksApi.ts
@@ -0,0 +1,10 @@
+import { baseApi } from "@/redux/api/baseApi";
+
+const authApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({}),
+});
+
+// api hooks
+// export const {
+
+// } = authApi;
