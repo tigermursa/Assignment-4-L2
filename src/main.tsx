761969bc3837432e31b49440c8b27daa2160a440
Mursalin <mursalin2311@gmail.com>
2025-07-13 03:24:39 +0600

    router and redux store setup in the main done

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { RouterProvider } from "react-router";
+import router from "./routes/index.ts";
+import { Provider } from "react-redux";
+import { store } from "./redux/store.ts";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router}></RouterProvider>
+    </Provider>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
---
+++ src/routes/index.ts
@@ -0,0 +1,12 @@
+import App from "@/App";
+import { createBrowserRouter } from "react-router";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: App,
+    children: [],
+  },
+]);
+
+export default router;
