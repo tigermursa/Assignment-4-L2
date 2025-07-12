0e8b1237bfd32ba129df09e1f0c68576313b4a90
Mursalin <mursalin2311@gmail.com>
2025-07-13 03:18:53 +0600

    ok created a structure of RTK crud api call in the booksApi file

--- src/redux/features/books/booksApi.ts
+++ src/redux/features/books/booksApi.ts
@@ -1,10 +1,60 @@
 import { baseApi } from "@/redux/api/baseApi";
 
 const authApi = baseApi.injectEndpoints({
-  endpoints: (builder) => ({}),
+  endpoints: (builder) => ({
+    //! create function
+    addBook: builder.mutation({
+      query: (data) => ({
+        url: `/api/v1/books`, //have to change
+        method: "POST",
+        body: data,
+      }),
+      invalidatesTags: ["data"],
+    }),
+
+    //! get function
+    getAllBook: builder.query({
+      query: () => ({
+        url: `/api/v1/books`,
+        method: "GET",
+      }),
+      providesTags: ["data"],
+    }),
+
+    //!get single with id function
+    getBookById: builder.query({
+      query: (id: string) => ({
+        url: `/api/v1/books/${id}`,
+        method: "GET",
+      }),
+      providesTags: ["data"],
+    }),
+
+    //!Update function
+    updateBook: builder.mutation({
+      query: (data) => ({
+        url: `/api/v1/books/${data}`,
+        method: "PUT",
+        body: data,
+      }),
+      invalidatesTags: ["data"],
+    }),
+
+    //! Delete function
+    deleteBook: builder.mutation({
+      query: (id: string) => ({
+        url: `/api/v1/books/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["data"],
+    }),
+  }),
 });
 
 // api hooks
-// export const {
-
-// } = authApi;
+export const {
+  useGetAllBookQuery,
+  useAddBookMutation,
+  useUpdateBookMutation,
+  useDeleteBookMutation,
+} = authApi;
